a67dc1428012676c1090cdb29e417f6f8ee80946
carteaga <carteaga@pragmainvest.com.bo>
2023-06-16 14:15:54 -0300

    Fix migration type

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
-Route::post('login', [\App\Http\Controllers\API\AuthController::class, 'login']);
+Route::post('login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
 
